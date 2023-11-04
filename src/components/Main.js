3172e929b2a20ee61c2b977456af9b48cfbe5513
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-11-04 09:19:56 +0330

    Validation function added and shown in UI

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React , {useState ,useEffect} from "react";
 import styles from "./Main.module.css";
+import validate from "../functions/validate";
 
 const Main = () => {
+    const [data,setData] = useState({
+        name:"",
+        lastName:"",
+        email:"",
+        phoneNumber: "" ,
+    })
+    const [error,setError] = useState("");
+
+    useEffect(()=> {
+        setError(validate(data))
+        console.log(error)
+    },[data])
+
+    const changeHandler = (event) => {
+        setData({...data , [event.target.name]:event.target.value})
+        console.log(data.name)
+    }
   return (
     <div className={styles.container}>
       <h2>Contact App</h2>
       <p>Save your Contacts here | Developed by Ehsan</p>
       <form className={styles.formContainer}>
         <div>
-          <input type="text" placeholder="Name" />
-          <input type="text" placeholder="Last Name" />
+          <input type="text" placeholder="Name" name="name" value={data.name} onChange={changeHandler} />
+          <input type="text" placeholder="Last Name" name="lastName" value={data.lastName} onChange={changeHandler}  />
         </div>
         <div>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+          <input type="email" placeholder="Email" name="email" value={data.email} onChange={changeHandler} />
+          <input type="phoneNumber" placeholder="PhoneNumber" name="phoneNumber" value={data.phoneNumber} onChange={changeHandler} />
         </div>
-        <button>Add Contact</button>
+        {error ? <span>{error}</span> : <button>Add Contact</button>}
       </form>
     </div>
   );
---
+++ src/functions/validate.js
@@ -0,0 +1,15 @@
+const validate = (data) => {
+    let error = "";
+    if(!data.name.trim() || !data.lastName.trim()){
+        error = "Enter Valid Data!";
+    }
+    else if(!/\S+@\S+\.\S+/.test(data.email)){
+        error = "Enter Valid Email!"
+    }
+    else if(data.phoneNumber.length !== 11 || !(+data.phoneNumber)) {
+        error = "Enter Valid Phone Number "
+    }
+    return error;
+}
+
+export default validate;
